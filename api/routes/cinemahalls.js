4196fa5eae8ce10fdd7050ba050db06592db5959
Thumas12 <thch19ab@student.cbs.dk>
2020-04-27 10:43:32 +0200

    gogogogog

--- api/routes/cinemahalls.js
+++ api/routes/cinemahalls.js
@@ -6,6 +6,8 @@ const CinemahallController = require('../controllers/cinemahalls');
 
 router.get('/', CinemahallController.Cinemahall_get_all)
 
+router.post('/findOne', CinemahallController.Cinemahall_get_one)
+
 router.post('/', CinemahallController.Cinemahalls_create_one)
 
 module.exports = router;
